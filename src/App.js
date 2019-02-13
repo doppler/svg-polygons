a3dd185ced48f1f9dd2d67bf5d46ed945320ab71
David Rose <doppler@gmail.com>
2019-02-13 13:49:45 -0500

    add slider to change # vertices

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Polygon from "./Polygon";
 import "./App.css";
 
 const App = () => {
-  const [baseHue, setBaseHue] = useState(48);
+  const [baseHue, setBaseHue] = useState(230);
+  const [vertices, setVertices] = useState(6);
   const handleHueSliderChange = event => setBaseHue(Number(event.target.value));
+  const handleVerticesSliderChange = event =>
+    setVertices(Number(event.target.value));
   return (
     <div id="App">
-      <Polygon {...{ baseHue }} />
+      <Polygon {...{ baseHue, vertices }} />
       <input
         type="range"
         value={baseHue}
@@ -15,6 +18,14 @@ const App = () => {
         min={0}
         max={359}
       />
+      <input
+        type="range"
+        value={vertices}
+        onChange={handleVerticesSliderChange}
+        min={3}
+        max={60}
+      />
+      <output>{JSON.stringify({ baseHue, vertices }, null, 2)}</output>
     </div>
   );
 };
