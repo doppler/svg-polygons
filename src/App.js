4a43f92c862bf3d7104734092960d56289ad2421
David Rose <doppler@gmail.com>
2019-02-13 13:16:15 -0500

    add slider to change baseHue

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const startHue = 30;
+  const [baseHue, setBaseHue] = useState(48);
+  const handleHueSliderChange = event => setBaseHue(Number(event.target.value));
   return (
     <div id="App">
       <svg
         height="500"
         width="500"
-        style={{ backgroundColor: `hsl(${startHue}, 100%, 50%)` }}
+        style={{ backgroundColor: `hsl(${baseHue}, 100%, 50%)` }}
       >
         <polygon
           points="100,10 40,198 190,78 10,78 160,198"
-          // style="fill:lime;stroke:purple;stroke-width:5;fill-rule:nonzero;"
           style={{
-            stroke: `hsl(${startHue + 180}, 100%, 50%)`,
-            fill: `hsl(${startHue + 120}, 100%, 50%)`,
+            stroke: `hsl(${baseHue + 180}, 100%, 50%)`,
+            fill: `hsl(${baseHue + 120}, 100%, 50%)`,
             strokeWidth: 5,
             fillRule: "nonzero"
           }}
         />
       </svg>
+      <input
+        type="range"
+        value={baseHue}
+        onChange={handleHueSliderChange}
+        min={0}
+        max={359}
+      />
     </div>
   );
 };
