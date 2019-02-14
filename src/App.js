e82506fd53866085093d9b7bd0e82ea454fca8d2
David Rose <doppler@gmail.com>
2019-02-13 21:24:35 -0500

    remove all the interactive shit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import React, { useState } from "react";
-import Polygon from "./Polygon";
+import React from "react";
 import "./App.css";
 
 const App = () => {
-  const [vertices, setVertices] = useState(6);
-  const handleVerticesSliderChange = event =>
-    setVertices(Number(event.target.value));
+  const size = 640;
+
+  const verticesPoints = vertices => {
+    return Array.from(Array(vertices))
+      .map((_, i) => i * (360 / vertices))
+      .map(angle => {
+        return [
+          size / 2 + (size / 2) * Math.cos((angle * Math.PI) / 180),
+          size / 2 + (size / 2) * Math.sin((angle * Math.PI) / 180)
+        ].join(",");
+      })
+      .join(" ");
+  };
+
   return (
     <div id="App">
-      <Polygon {...{ vertices }} />
-      <input
-        type="range"
-        value={vertices}
-        onChange={handleVerticesSliderChange}
-        min={3}
-        max={60}
-      />
-      <output>{JSON.stringify({ vertices }, null, 2)}</output>
+      <svg height={size} width={size} style={{ transform: "rotate(-90deg)" }}>
+        {Array.from({ length: 13 }, (x, i) => i)
+          .slice(3, 13)
+          .reverse() // haha, could've just done [12,11,10,9,8,7,6,5,4,3]
+          .map(v => (
+            <polygon
+              points={verticesPoints(v)}
+              style={{
+                stroke: "white",
+                strokeWidth: 1,
+                fill: `hsla(${360 / v}, 100%, 50%, 0.33)`
+              }}
+            />
+          ))}
+      </svg>
     </div>
   );
 };
